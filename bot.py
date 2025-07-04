313ae4ca429a4e85ef6c0f0d39fef932779f7dae
onur11185 <onuraydinrasullu61@gmail.com>
2025-07-04 14:38:22 +0300

    commit11

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import discord
 from discord.ext import commands
-from config import token  # Botun tokenini config dosyasından içe aktarma
+from config import token  
 
 intents = discord.Intents.default()
-intents.members = True  # Botun kullanıcılarla çalışmasına ve onları banlamasına izin verir
+intents.members = True  
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -34,5 +34,21 @@ async def ban_error(ctx, error):
         await ctx.send("Bu komutu çalıştırmak için yeterli izniniz yok.")
     elif isinstance(error, commands.MemberNotFound):
         await ctx.send("Kullanıcı bulunamadı!")
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if "https://" in message.content:
+        try:
+            await message.guild.ban(message.author, reason="Link Gönderdi")
+            await message.channel.send(f"{message.author.mention} link gönderdiği için banlandı.")
+        except Exception as e:
+            await message.channel.send(f"Ban başarısız: {e}")
+        return  
+
+    await bot.process_commands(message)
 
 bot.run(token)
